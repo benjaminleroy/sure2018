cf3e6d148bfd49a0f2365108516725217dffc6a3
shiva-darshan <sd2997@nyu.edu>
2018-07-09 23:59:30 -0400

    A function to do a sense test for the number of 1's in each row and block after imputation

---
+++ functions/some_testing_functions.R
@@ -0,0 +1,35 @@
+#' Sense test for the number of 1's in for a given set of variables 
+#'
+#' @param df a data frame
+#' @param var_cols a vector of column indicies or names for each variable in the 
+#' set
+#' @param a a number which each row will be compared to
+#' @param equal a logical, if true the function will return the number of rows 
+#' with 'a' 1's in the set of variables
+#' @param number a logical, if true the function will return the number 1's in 
+#' all rows in the set of variables
+#' @param less_than a logical, if true the function will return the number of rows 
+#' with less than 'a' 1's in the set of variables
+#' @param greater_than a logical, if true the function will return the number of rows 
+#' with more than 'a' 1's in the set of variables
+#'
+#' @return depends on the which parameter is set equal to true
+#' @export
+#'
+#' @examples
+row_sum_test <- function(df, var_cols, a, equal = FALSE, number = FALSE, 
+                         less_than = !greater_than, greater_than = !less_than) {
+  
+  row_sums <- apply(df[,var_cols], MARGIN = 1, function(x){x %>% as.numeric %>% sum})
+  
+  if (equal) {
+    row_sums %>% function(x) {x == a} %>% sum %>% return
+  } elif (number) {
+    row_sums %>% sum %>% return
+  } elif (less_than) {
+    row_sums %>% function(x) {x < a} %>% sum %>% return
+  } elif (greater_than) {
+    row_sums %>% function(x) {x < a} %>% sum %>% return
+  }
+}
+
