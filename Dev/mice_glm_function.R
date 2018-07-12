e3a70613ddfe0c702c9d0396aed98a4d46569280
icarson2 <imanic.carson@gmail.com>
2018-07-11 23:40:05 -0400

    The beginnings of glm function

---
+++ Dev/mice_glm_function.R
@@ -0,0 +1,47 @@
+
+
+# beta_values<-matrix()
+# 
+# recruiter<- trimmed_dataset[67:71]
+# 
+# glm_shell <- function(x){
+#  for (i in 1:5){
+#   mice$i<- glm_df
+# #look at coumentation to see how accessing the dataframe works   
+#   logit_fun <- glm(y ~ + ageBroad + recruiter+ age::gender  data=glm_df)
+#   
+#   
+#  } 
+# }
+
+
+#fakedataset
+isLabour = c(0,NA,0,1,1)
+ageBroad = c("18--20","21--23","18--20","18--20","21-23")
+gender = c("Female","Male","Female","Female","Female")
+
+
+recruiterRelationOther = c(0,0,1,NA,1)
+
+recruiterRelationUknown = c(1,1,0,0,0)
+
+df <- data.frame(isLabour,ageBroad,gender,recruiterRelationOther,recruiterRelationUknown)
+
+mice_sep = funcion(mno){
+  mno$datalist[1]
+  
+  
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
